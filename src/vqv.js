4e919d0b7b15c8fd501e09c17983ce2623ad407e
Marcos Pimentel <mpc_marcos@hotmail.com>
2022-07-19 20:43:29 -0300

    Requisito 3 - Completo

--- src/vqv.js
+++ src/vqv.js
@@ -19,9 +19,12 @@
 */
 
 const vqv = (name, age) => {
-  return `Oi, meu nome é ${name}!
+  if (typeof name === "string" && typeof age === "number") {
+    return `Oi, meu nome é ${name}!
 Tenho ${age} anos,
 trabalho na Trybe e mando muito em programação!
 #VQV!`;
+  }
+  return undefined;
 };
 module.exports = vqv;
